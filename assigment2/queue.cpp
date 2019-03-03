2fa5a7b8758c2521a4e6fd6a3cc25fd388fc8ad4
jinqiu <1491716282@qq.com>
2019-03-03 20:38:13 +0800

    commit1

--- assigment2/queue.cpp
+++ assigment2/queue.cpp
@@ -29,6 +29,37 @@ int main()
     cout<<"back:"<<list_queue.back()<<endl;
     cout<<"pop:"<<list_queue.pop()<<endl;
     list_queue.print();
+
+    cout<<"=========>CyclicQueue:"<<endl;
+    CyclicQueue<int> cyclic_queue(10);
+    for(int i=0;i<10;++i){
+        cyclic_queue.push(i);
+    }
+    cyclic_queue.print();
+    cyclic_queue.push(11);
+    cyclic_queue.print();
+    cout<<"max size:"<<cyclic_queue.max_size()<<endl;
+    cout<<"size:"<<cyclic_queue.size()<<endl;
+    cout<<"front:"<<cyclic_queue.front()<<endl;
+    cout<<"back:"<<cyclic_queue.back()<<endl;
+    cout<<"pop:"<<cyclic_queue.pop()<<endl;
+    cyclic_queue.print();
+
+    CyclicQueue<int> cyclic_queue1(10);
+    for(int i=0;i<10;++i){
+        cyclic_queue1.push(i);
+    }
+    cyclic_queue1.print();
+    cyclic_queue1.pop();
+    cyclic_queue1.pop();
+    cyclic_queue1.print();
+    cyclic_queue1.push(19);
+    cyclic_queue1.push(20);
+    cout<<"size:"<<cyclic_queue1.size()<<endl;
+    cyclic_queue1.print();
+
+
+
     return 0;
 }
 
--- assigment2/queue.h
+++ assigment2/queue.h
@@ -205,7 +205,149 @@ void ListQueue<T>::print()
 template <class T>
 class CyclicQueue:public Queue<T>
 {
-    
+public:
+    CyclicQueue():capacity_(10),
+                  ptr_(shared_ptr<T>(new T[capacity_],[](T *p){delete[] p;})),
+                  size_(0),
+                  front_(ptr_.get()),
+                  back_(ptr_.get()){}
+    CyclicQueue(size_t capacity);
+    ~CyclicQueue(){}
+
+    //队列是否为空
+    bool empty(){return size_==0;}
+    //队列的大小
+    size_t size(){return size_;}
+    //队列容量大小
+    size_t max_size(){return capacity_;}
+
+    //返回队首元素
+    T front();
+    //返回队尾元素
+    T back();
+    //队首弹出元素
+    T pop();
+    //队尾添加元素
+    void push(const T &value);
+
+    //打印队列
+    void print();
+private:
+    void expend_capacity(const size_t &new_capacity);//动态扩容
+private:
+    shared_ptr<T> ptr_;
+    T *front_;//队列头指针
+    T *back_;//队列尾指针
+    size_t  size_;//队列中存储元素的个数
+    size_t  capacity_;//队列中最大存储数量
 };
+
+template <class T>
+CyclicQueue<T>::CyclicQueue(size_t capacity)
+{
+    if(capacity_<=0){
+        cout<<"queue capacity must >0"<<endl;
+        exit(0);
+    }
+
+    capacity_=capacity;
+    size_=0;
+    ptr_=shared_ptr<T>(new T[capacity_],[](T *p){delete[] p;});
+    front_=back_=ptr_.get();
+}
+
+template <class T>
+T CyclicQueue<T>::front()
+{
+    if(empty()){
+        cout<<"CyclicQueue is empty!"<<endl;
+        exit(0);
+    } else{
+        return *front_;
+    }
+}
+
+template <class T>
+T CyclicQueue<T>::back()
+{
+    if(empty()){
+        cout<<"CyclicQueue is empty!"<<endl;
+        exit(0);
+    } else{
+        return *back_;
+    }
+}
+
+template <class T>
+T CyclicQueue<T>::pop()
+{
+    if(empty()){
+        cout<<"CyclicQueue is empty!"<<endl;
+        exit(0);
+    }
+
+    T value=*front_;
+    if(front_==ptr_.get()+capacity_-1){
+        front_=ptr_.get();
+    }else {
+        ++front_;
+    }
+    --size_;
+    return value;
+}
+
+template <class T>
+void CyclicQueue<T>::expend_capacity(const size_t &new_capacity)
+{
+    shared_ptr<T> temp(new T[new_capacity],[](T *p){delete[] p;});
+
+    copy(ptr_.get(),ptr_.get()+size_,temp.get());
+    ptr_.swap(temp);
+    front_ = ptr_.get();
+    back_ = ptr_.get()+size_-1;
+    temp.reset();
+}
+
+template <class T>
+void CyclicQueue<T>::push(const T &value)
+{
+    if(size_==0)
+    {
+        *back_=value;
+    }else if(size_==capacity_){//如果队列已满，需要进行扩容
+        capacity_*=2;
+        expend_capacity(capacity_);
+
+        ++back_;
+        *back_=value;
+    }else{
+        size_t distance=back_-ptr_.get();//获取尾元素的位置
+        size_t index=(distance+1)%capacity_;//获取插入后元素的位置
+        back_=ptr_.get()+index;
+        *back_=value;
+    }
+
+    ++size_;
+}
+
+template <class T>
+void CyclicQueue<T>::print()
+{
+    if(size_==0){
+        return ;
+    }
+    size_t i=front_-ptr_.get();
+    size_t j=back_-ptr_.get();
+    while(true){
+        int index=i%capacity_;
+        if(index==j){
+            break;
+        }
+        cout<<*(ptr_.get()+index)<<" ";
+        ++i;
+    }
+
+    cout<<*back_<<endl;
+}
 
 #endif //TESK_QUEUE_H
