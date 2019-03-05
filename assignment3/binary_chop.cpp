bc37323d9c3bcddc3a2cd0a409bfd473d34038e5
jinqiu <1491716282@qq.com>
2019-03-05 22:55:00 +0800

    commit1

--- assignment3/binary_chop.cpp
+++ assignment3/binary_chop.cpp
@@ -16,7 +16,7 @@ int binary_search(int arr[],int start,int end,int target)
     if(target>arr[end] || target<arr[start]){
         exit(0);
     }
-    int i=(end-start)/2;
+    int i=(end+start)/2;
     if(arr[i]==target){
         return i;
     }else if(arr[i]>target){
@@ -29,12 +29,38 @@ int binary_search(int arr[],int start,int end,int target)
 /*
  * 实现模糊二分查找算法（比如大于等于给定值的第一个元素）
  * */
+int blur_binary_chop(int arr[],int len,int target)
+{
+    int start=0;
+    int end=len-1;
+    int mid=0;
+    while(true){
+        if(target<arr[start] || target>arr[end]){
+            exit(0);
+        }
 
+        mid=(start+end)/2;
+
+        if(arr[mid]>target){
+            end=mid;
+        }
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]<target){
+            start=mid;
+        }
+        if(end-start==1 && target<arr[end] && target>arr[start]){
+            return end;
+        }
+    }
+}
 
 int main()
 {
-    int arr[5]={1,2,3,4,5};
-    cout<<binary_search(arr,0,4,1)<<endl;
+    int arr[5]={1,2,3,5,7};
+    cout<<binary_search(arr,0,4,5)<<endl;
+    cout<<blur_binary_chop(arr,5,4)<<endl;
 
     return 0;
 }
