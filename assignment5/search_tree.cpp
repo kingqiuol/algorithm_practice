9ea733008517a86d1aeefcd5f6c6a088a4f04be0
kingqiuol <1491716282@qq.com>
2019-04-24 10:53:56 +0800

    commit1

--- assignment5/search_tree.cpp
+++ assignment5/search_tree.cpp
@@ -1,10 +1,22 @@
 //
 // Created by jinqiu on 19-4-21.
 //
-
+#include <iostream>
+#include <vector>
 #include "search.h"
+
+using namespace std;
 
 int main()
 {
+    vector<pair<int,int>> a,b;
+
+    b.push_back(make_pair(0,1));
+    b.push_back(make_pair(1,2));
+
+    for(auto c:b){
+        cout<<c.first<<" "<<c.second<<endl;
+    }
+
     return 0;
 }
